d6d12483dd4205e5efcc86948c707f4031bb650f
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-08 22:17:01 +0100

    New reference

--- references.js
+++ references.js
@@ -1,2 +1,3 @@
 // Gradient effect inspiration https://www.youtube.com/watch?v=-MUOweQ6wac
 // Inspiration to generative art objects https://www.youtube.com/watch?v=SGHWZz5Mrsw
+// Interesting water effect https://www.youtube.com/watch?v=H7CEy5mgKFY
