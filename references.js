eaca9dd8ea5249435bee81859c162bc54cb8c1e5
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-08 22:20:38 +0100

    Interesting videos about concepts we might use

--- references.js
+++ references.js
@@ -1,3 +1,6 @@
 // Gradient effect inspiration https://www.youtube.com/watch?v=-MUOweQ6wac
 // Inspiration to generative art objects https://www.youtube.com/watch?v=SGHWZz5Mrsw
 // Interesting water effect https://www.youtube.com/watch?v=H7CEy5mgKFY
+// Bees and Bombs concept https://www.youtube.com/watch?v=H81Tdrmz2LA
+// Wave Function Collapse algorithm https://www.youtube.com/watch?v=2SuvO4Gi7uY
+// Wave Function Collapse https://www.youtube.com/watch?v=rI_y2GAlQFM
