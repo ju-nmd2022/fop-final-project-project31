3cd909a44ca7868a2c9f367152b52aadb389fb03
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-10 12:06:15 +0100

    reference

--- references.js
+++ references.js
@@ -4,3 +4,4 @@
 // Bees and Bombs concept https://www.youtube.com/watch?v=H81Tdrmz2LA
 // Wave Function Collapse algorithm https://www.youtube.com/watch?v=2SuvO4Gi7uY
 // Wave Function Collapse https://www.youtube.com/watch?v=rI_y2GAlQFM
+// Simple sine wave animation https://www.youtube.com/watch?v=ktPnruyC6cc
