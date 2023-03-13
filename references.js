2cabb64bb589a12f9367c05d6950da55e263704f
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-13 12:23:22 +0100

    Reference

--- references.js
+++ references.js
@@ -6,3 +6,4 @@
 // Wave Function Collapse https://www.youtube.com/watch?v=rI_y2GAlQFM
 // Simple sine wave animation https://www.youtube.com/watch?v=ktPnruyC6cc
 //Let's Cook Math Donuts in p5.js https://www.youtube.com/watch?v=iNA4yH7DAN8
+//Similar Game https://codepen.io/MillerTime/pen/BexBbE
