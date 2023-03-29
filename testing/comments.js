a3bf9845c9e0e1e3a89e32b17043f1229579fd88
velklusia <116886960+velklusia@users.noreply.github.com>
2023-03-29 12:35:26 +0200

    comments

---
+++ testing/comments.js
@@ -0,0 +1,16 @@
+//check the load pixels in documentation
+//slice the array with the last 50 instead of having maxPoints
+// check pixels array in the documentation
+// because of the density * canvasWidth and density 
+// instead of push use
+// we are going throgh the array 
+// the new points should have been added at the beginning of the array and then we do not need the if statement at all!
+// pgs are added in the objects.js
+//we can use objects of objects instead of for let that creates graphics in objects.js
+// when is on mouse up and when we are actually drawing on the pg is the problem, it draws it draws it draws but we do not control the event as much 
+
+// the main problem is that we created the canvas but we are checking sth on the object instead'
+// look up the create image in the documentation
+// we do minus because we check where do we take the graphics from
+
+// take the offscreen canvas 
